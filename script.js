f661837c0f8fb451e1cffb67b26c60b6a5655ba9
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-24 16:08:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('canvas');
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    let selectedColor = colors[0]; // Default color
+
+    // Create color picker
+    const colorPicker = document.getElementById('color-picker');
+    colors.forEach(color => {
+        const colorDiv = document.createElement('div');
+        colorDiv.classList.add('color');
+        colorDiv.id = color;
+        colorDiv.style.backgroundColor = color;
+        colorDiv.addEventListener('click', () => selectColor(color));
+        colorPicker.appendChild(colorDiv);
+    });
 
     // Create the initial canvas grid
     for (let i = 0; i < 10000; i++) {
@@ -10,7 +23,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function drawPixel(event) {
-        const selectedColor = 'red'; // Change this to allow different colors
         event.target.style.backgroundColor = selectedColor;
+    }
+
+    function selectColor(color) {
+        selectedColor = color;
     }
 });
