c73943e3392f952c62fd472bf4ea8f248eff9ea5
Bro112301 <142183375+Bro112301@users.noreply.github.com>
2023-11-24 15:51:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    const canvas = document.getElementById('canvas');
+
+    // Create the initial canvas grid
+    for (let i = 0; i < 10000; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.addEventListener('mouseover', drawPixel);
+        canvas.appendChild(pixel);
+    }
+
+    function drawPixel(event) {
+        const selectedColor = 'red'; // Change this to allow different colors
+        event.target.style.backgroundColor = selectedColor;
+    }
+});
